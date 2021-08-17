a387a0cb6d175190ee3cf8ce30707286a2eb3bbb
Mahdi Mosazadeh <mahdimosazadeh1997@gmail.com>
2021-08-17 16:01:50 -0700

    This is serial connection with lcd v.1.0

---
+++ finallySerial.c
@@ -0,0 +1,66 @@
+/*
+ * serial_connection.c
+ *
+ * Created: 6/2/2019 2:27:57 PM
+ * Author: pers
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
+#include <mega32.h>
+#include <delay.h>
+
+void main(void)
+{
+    char recive[1];
+    int number;
+    
+    DDRB = 0b00001111;    
+    //set serial connection on micro controller
+    UCSRA = 0x00;
+    UCSRB = 0x10; // if mircro is sender value 0x08 if reciver 0x10
+    UCSRC = 0x86;
+    //set serial speed on 9600
+    UBRRH = 0x00;
+    UBRRL = 0x33;
+    
+while (1)
+    {
+        gets(recive,1);
+        number=atoi(recive);//atoi --> change char to int. itoa --> change int to char.
+        
+        //button 1
+        if (number==1)
+        {
+            PORTB = 0b00000001; 
+            delay_ms(1000);
+            PORTB = 0b00000000;
+            
+        }
+        
+        //button 2
+        if (number==2)
+        {
+            PORTB = 0b00000010;
+            delay_ms(1000);
+            PORTB = 0b00000000; 
+        }
+        
+        //button 3
+        if (number==3)
+        {
+            PORTB = 0b00000100; 
+            delay_ms(1000);
+            PORTB = 0b00000000; 
+        }
+        
+        //button 4
+        if (number==4)
+        {
+            PORTB = 0b00001000; 
+            delay_ms(1000);
+            PORTB = 0b00000000; 
+        }          
+    }
+}
